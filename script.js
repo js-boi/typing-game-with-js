1f452d4c67abef6f69c39ff12b1a6b780d376495
tsuboi <jsboi1030@gmail.com>
2022-03-04 12:58:07 +0900

    [add] ランダムな文章を取得して、HTML要素に表示

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+const typeDisplay = document.getElementById("typeDisplay");
 
 // 非同期でランダムな文章を取得する
 function GetRandomSentence() {
@@ -8,9 +9,12 @@ function GetRandomSentence() {
 }
 
 // ランダムな文章を取得して、表示する
-function RenderNextSentence() {
-    const sentence = GetRandomSentence();
+async function RenderNextSentence() {
+    const sentence = await GetRandomSentence();
     console.log(sentence);
+    typeDisplay.innerText = sentence;
+
+    // 文章を1文字ずつ分解して、spanタグを生成する
 }
 
 RenderNextSentence();
