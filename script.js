1c2008e4bb44b885592d4c46abf49c13f46aa5c6
tsuboi <jsboi1030@gmail.com>
2022-03-04 12:51:20 +0900

    [add] ランダムな文章を取得して、コンソール表示

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("unko");
+const RANDOM_SENTENCE_URL_API = "https://api.quotable.io/random";
+
+// 非同期でランダムな文章を取得する
+function GetRandomSentence() {
+    return fetch(RANDOM_SENTENCE_URL_API)
+        .then((response) => response.json())
+        .then((data) => data.content);
+}
+
+// ランダムな文章を取得して、表示する
+function RenderNextSentence() {
+    const sentence = GetRandomSentence();
+    console.log(sentence);
+}
+
+RenderNextSentence();
+
+
+
+
